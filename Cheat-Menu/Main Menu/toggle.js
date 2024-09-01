cca8abb68718f4824252d9ca42160732443bcd63
Dragon_X <barron.samuel111@gmail.com>
2024-09-01 12:26:31 -0700

    Create toggle.js

---
+++ Cheat-Menu/Main Menu/toggle.js
@@ -0,0 +1,7 @@
+(function() {
+    // Toggle switch functionality
+    document.getElementById('toggleTeleport').addEventListener('click', function() {
+        this.classList.toggle('checked');
+        // Add your functionality for the toggle switch here
+    });
+})();
