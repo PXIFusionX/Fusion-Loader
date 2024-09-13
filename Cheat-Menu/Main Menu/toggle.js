352a9753b333c0c0f2d7de5947c16bd97fc89074
PXIFUSIONDEV214 <repcockd@gmail.com>
2024-09-12 19:05:11 -0500

    Update toggle.js

--- Cheat-Menu/Main Menu/toggle.js
+++ Cheat-Menu/Main Menu/toggle.js
@@ -1,7 +1,1 @@
-(function() {
-    // Toggle switch functionality
-    document.getElementById('toggleTeleport').addEventListener('click', function() {
-        this.classList.toggle('checked');
-        // Add your functionality for the toggle switch here
-    });
-})();
+
